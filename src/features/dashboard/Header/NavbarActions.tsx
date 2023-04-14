af85296717cc4cc0319803694cd293d6ac1d6d3b
Willian Lemann <willianleman@hotmail.com>
2023-04-14 20:20:16 -0300

    feat: remove search

--- src/features/dashboard/Header/NavbarActions.tsx
+++ src/features/dashboard/Header/NavbarActions.tsx
@@ -9,24 +9,6 @@ import { classnames } from "@/utils/classnames";
 import { CartIcon } from "@/components/CartIcon";
 
 export const NavbarActions = () => {
-  const { totalItems, toggle } = useCart();
-  const [isInputVisible, setIsInputVisible] = useState(false);
-  const [search, setSearch] = useState("");
-
-  const handleInputBlur = () => {
-    if (!search) {
-      return setIsInputVisible(false);
-    }
-
-    setIsInputVisible(true);
-  };
-
-  useEffect(() => {
-    if (search.length === 0) {
-      setIsInputVisible(false);
-    }
-  }, [search]);
-
   return (
     <div className="ml-auto flex items-center">
       <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
@@ -37,28 +19,6 @@ export const NavbarActions = () => {
         <a href="#" className="text-sm font-medium">
           Create account
         </a>
-      </div>
-
-      <div className="flex lg:ml-6 lg:max-w-full">
-        <a
-          href="#"
-          className="p-2"
-          onClick={() => setIsInputVisible(!isInputVisible)}
-        >
-          <MagnifyingGlassIcon className="h-6 w-6" aria-hidden="true" />
-        </a>
-
-        <input
-          type="text"
-          placeholder="Search"
-          value={search}
-          onChange={({ target }) => setSearch(target.value)}
-          onBlur={handleInputBlur}
-          className={classnames(
-            "rounded-md border px-2 text-gray-400 outline-none",
-            isInputVisible ? "max-w-full" : "sr-only max-w-0"
-          )}
-        />
       </div>
 
       <CartIcon />
